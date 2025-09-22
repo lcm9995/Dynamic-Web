1c512381bbb2147a5cd1fa31ab30ef00b047f07f
lcm9995 <lcm9995@nyu.edu>
2025-09-22 15:08:47 -0400

    add recipe card as a folder

---
+++ recipe_card/src/App.js
@@ -0,0 +1,29 @@
+import Card from './components/Card'
+import RecipeInfo from './components/RecipeInfo'
+import IngredientsList from './components/IngredientsList'
+import InstructionsList from './components/InstructionsList'
+import RecipeImg from './components/RecipeImg'
+import './components/RecipeCard.css'
+import Counter from './components/Counter'
+import {RECIPE} from './components/recipe_data'
+
+const App = () => {
+  return (
+    <div>
+      <Card>
+        <RecipeImg imgSrc={RECIPE.imgSrc} />
+        <div className="card_text">
+          <RecipeInfo title={RECIPE.title} description={RECIPE.description} />
+          <div className="card_lists">
+            <IngredientsList data={RECIPE.ingredients} />
+            <InstructionsList data={RECIPE.instructions} />
+          </div>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default App
+
+//#whenever we say export default, we don't need curly braces when we import that component 
